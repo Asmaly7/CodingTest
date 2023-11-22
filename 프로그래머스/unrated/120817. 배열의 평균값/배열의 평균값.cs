3af14d6e9961c58e5e691c1992d3387b6bea6494
Asmaly7 <148426887+Asmaly7@users.noreply.github.com>
2023-11-23 08:52:39 +0900

    [level unrated] Title: 배열의 평균값, Time: 0.17 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120817. 배열의 평균값/배열의 평균값.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution 
+{
+    public double solution(int[] numbers)
+    {
+        double sum = 0;
+        
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+        
+        double average = sum / numbers.Length;
+        return average;
+    }
+}
